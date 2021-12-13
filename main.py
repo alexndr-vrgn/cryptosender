915f8b57f6cce7ad4d66eddf257778d11ef843cc
alexndr-vrgn <73052248+alexndr-vrgn@users.noreply.github.com>
2021-12-13 23:34:00 +0300

    created file request.py for requests to CMC_API

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from configs.config_for_cryptobot import config as cfg
+from modules import request
+import telebot
+import time
+import pymysql.cursors
+import babel.numbers as bn
+import schedule
+
+
+
+
---
+++ modules/request.py
@@ -0,0 +1,3 @@
+import requests
+import json
+from configs.config_for_cryptobot import config as cfg
